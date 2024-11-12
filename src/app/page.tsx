77b2a71cef5649f11e21563f28121094cdcd46d2
Richard Beindorf <richard_beindorf@web.de>
2024-11-12 16:25:02 +0100

    new project works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,87 @@
+"use client";
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import styled from "styled-components";
+// import Frame1 from "../../public/Luffy1.png";
+// import Frame2 from "../../public/Luffy2.png";
+
+export const ImageContainer = styled.div`
+    width: 1400px;
+    height: 800px;
+`;
+
+// const Frontal1 = styled(Image)`
+//     position: absolute;
+//     top: 0px;
+//     left: 0px;
+// `;
+
+// const Frontal2 = styled(Frontal1)`
+//     position: absolute;
+//     top: 0px;
+//     left: 0px;
+// `;
+
+// Idee: wenn ich die Bilder auf eigene Anweisung hin laden und damit rendern lassen kann, kontrolliere ich die render-Last
+// class UrlContainer {
+//   [key: number]: string;
+//   constructor(url){
+//   }
+// }
+//   const imageLoader = new UrlContainer {
+//   };
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	// der counter erhöht sich basierend auf den aktuellen scorll punkt
+	// vielleicht zunächst mit pfeiltasten weiter machen lassen (input ist schwer)
+	// const KEY_HANDLERS = {
+	// 	ArrowLeft: () => console.log("You pressed the left Arrow."),
+	// 	ArrowRight: () => console.log("You pressed the right Arrow."),
+	// };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	// function frameCounter() {
+	// 	let counter = 0;
+	// 	if (counter < 24) {
+	// 		counter += 1;
+	// 	}
+
+	// 	return counter;
+	// }
+
+	// const urlParameter = `../../public/Luffy${frameCounter()}`;
+
+	// function onKeydown() {
+	// 	document.addEventListener("keydown", (event) => {
+	// 		event.preventDefault();
+
+	// 		const handler = KEY_HANDLERS[event.code];
+
+	// 		if (handler) {
+	// 			handler();
+	// 			return;
+	// 		}
+
+	// 		console.log("Pressed a key without a handler.");
+	// 		// return event.key;
+	// 	});
+
+	// }
+	return (
+		<ImageContainer>
+			<Image
+				src={"/assets/Background1.png"}
+				alt="backdrop"
+				width={1400}
+				height={800}
+				priority
+			/>
+			{/* <Frontal1
+				src={Frame1}
+				alt="backdrop"
+				width={1200}
+				height={600}
+				priority
+			/> */}
+		</ImageContainer>
+	);
 }
