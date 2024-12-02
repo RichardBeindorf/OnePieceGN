aae9e96612d3a7f4b608e6c8cd9a16b4f86f7ed1
Richard Beindorf <richard_beindorf@web.de>
2024-12-02 19:59:33 +0100

    first frame border styled

--- src/app/components/bleedOutElement.tsx
+++ src/app/components/bleedOutElement.tsx
@@ -1,37 +1,98 @@
 import styled from "styled-components"
 
-const OuterBorder = styled.div`
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: fit-content;
+    padding: 10px;
+`;
+
+
+const Limiter = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 240px;
+    width: 50px;
+    background-image: var(--paperBG);
+    /* background-color: hotpink; */
+    transform: translate(-30px, 10px) rotate(-3deg);
+    rotate: calc(10deg);
+    position: relative;
+    z-index: 110;
+
+    /* &:before {
+        content: "";
+        position: absolute;
+        transform: translate(-37px, 110px) rotate(8deg);
+        background-image: var(--paperBG);
+        /* background-color: hotpink; */
+        /* border-radius: 15%; */
+        /* border: 2px solid black; */
+        /* height: 250px;
+        width: 50px;
+        margin-left: 5px; */
+    /* } */ 
+
+
+    &:before{
+        content: "";
+        position: absolute;
+        transform: translate(-25px, 9px) rotate(-10deg);
+        rotate: calc(10deg);
+        border-right: solid 3px #41403E;
+        border-top-right-radius: 10px 90px;
+
+
+        border-bottom: solid 3px #41403E;
+        border-bottom-right-radius: 10px 80px;
+
+
+        /* background-image: var(--paperBG); */
+        /* background-color: hotpink; */
+        height: 210px;
+        width: 10px;
+        margin-left: 5px;
+        z-index: 100;
+    }
+
+`;
+
+const Border = styled.div`
+    box-sizing: content-box;
     display: flex;
     justify-content: center;
     align-items: center;
     //Dimensions
     height: 200px;
     width: 400px;
-    border-top: 2.5px solid black;
-    border-left: 2.5px solid black;
-    border-bottom: 2.5px solid black;
+    border-top: solid 3px #41403E;
+    border-left: solid 3px #41403E;
+    border-bottom: solid 3px #41403E;
+    border-top-left-radius: 255px 15px;
+    /* border-top-right-radius: 15px 300px; */
+    border-bottom-right-radius: 225px 15px;
+    border-bottom-left-radius:15px 255px;
     padding: 5px;
     margin: 20px;
+    overflow: hidden;
+  
 
-    &:after{
-        content: "";
-        position: relative;
-        transform: translate( 100px, -21px);
-        rotate: calc(10deg);
-        border-right: 2px solid black;
-        /* background-color: hotpink; */
-        height: 200px;
-        width: 50px;
-        margin-left: 5px;
-    }
+
 `;
+
 
 
 
 export default function BleedOutElement (){
     return (
-        <OuterBorder>
+        
+        <Wrapper>
+        <Border>
             <h5>Hier kommt das Ruffy Intro hin</h5>
-        </OuterBorder>
+        </Border>
+        <Limiter/>
+        </Wrapper>
+        
     )
 }
